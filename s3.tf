0c8adacf7129f7ee14a2756909287fffd71d8d9a
PoojaMulay <119414915+PoojaMulay@users.noreply.github.com>
2023-09-14 16:09:30 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-  terraform {
+/*terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "tf-state-bucket-demo"
@@ -7,5 +7,19 @@
     # Replace this with your DynamoDB table name!
     dynamodb_table = "cicd-demo-table"
     encrypt        = true
+  }
+} */
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-bucket-demo"  
+    key            = "terraform.tfstate" 
+    region         = "us-east-1"        
+
+    profile        = my_default_profile_name    
+
+    encrypt        = true               
+    dynamodb_table = "terraform-lock"   
+
   }
 }
